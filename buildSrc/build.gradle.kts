ad4ecbf00b0b95c4a45f226e28c1096613064a14
Kenechukwu Akubue <kengoon19@gmail.com>
2023-12-04 12:47:08 +0100

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.kgp)
+    implementation(libs.agp)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
