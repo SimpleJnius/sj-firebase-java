b32bd460abc46744cff7a85add5a261a17d283e8
Kenechukwu Akubue <kengoon19@gmail.com>
2023-12-08 18:14:43 +0100

    Update version
    Signed-off-by: Kenechukwu Akubue <kengoon19@gmail.com>

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -8,7 +8,7 @@ object AppCoordinates {
 }
 
 object LibraryAndroidCoordinates {
-    const val LIBRARY_VERSION = "0.1.0"
+    const val LIBRARY_VERSION = "0.2.0"
     const val LIBRARY_VERSION_CODE = 1
 }
 
