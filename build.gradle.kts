f579cbc8b71bd24e8d2c35175d2e8143ccc407fc
Kenechukwu Akubue <kengoon19@gmail.com>
2023-12-19 03:20:16 +0100

    Update build.gradle.kts
    Signed-off-by: Kenechukwu Akubue <kengoon19@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
-    kotlin("android") apply false
+    // kotlin("android") apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.versions)
     base
