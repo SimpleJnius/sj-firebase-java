ed381c1ad7b6acc040ee61c0c8ff23fcb607a56a
Kenechukwu Akubue <kengoon19@gmail.com>
2023-12-19 03:34:35 +0100

    Update build.gradle.kts
    Signed-off-by: Kenechukwu Akubue <kengoon19@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,11 +16,11 @@ dependencies {
     implementation(libs.agp)
 }
 
-// tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-//    kotlinOptions {
-//        jvmTarget = JavaVersion.VERSION_17.toString()
-//    }
-// }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
 
 // kotlin {
 //    jvmToolchain(17)
