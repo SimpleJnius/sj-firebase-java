cd74bf0f782b234d2fc11ab35f5ca75a4d9f5dd5
Kenechukwu Akubue <kengoon19@gmail.com>
2025-02-19 18:38:47 +0100

    version update

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -8,7 +8,7 @@ object AppCoordinates {
 }
 
 object LibraryAndroidCoordinates {
-    const val LIBRARY_VERSION = "1.3.0"
+    const val LIBRARY_VERSION = "1.3.1"
     const val LIBRARY_VERSION_CODE = 1
 }
 
